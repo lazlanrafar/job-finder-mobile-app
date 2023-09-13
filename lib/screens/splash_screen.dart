403d574417766b9f4aa7c98384bf663a5436ddcc
lazlanrafar <lazlanrafar@gmail.com>
2023-09-13 13:00:26 +0700

    slicing splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -8,36 +8,46 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: defaultMargin,
-          vertical: 32,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.start,
+      body: SafeArea(
+        child: Stack(
           children: [
-            Text(
-              "Find the Job You've\nAlways Dreamed of",
-              style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w500,
-                  color: primaryColor),
+            Image.asset(
+              'assets/bg_splash.png',
+              fit: BoxFit.fitWidth,
+              width: double.infinity,
             ),
-            const SizedBox(height: 8),
-            Text(
-              "One of the places where you will find the right job with your field of interest, and you just have to wait for the manager to call you to hire",
-              style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w300,
-                  color: primaryColor),
-            ),
-            const SizedBox(height: 73),
-            Button(text: 'Get Started', onPressed: () => {})
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Find the Job You've\nAlways Dreamed of",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                      color: primaryColor,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    "One of the places where you will find the right job with your field of interest, and you just have to wait for the manager to call you to hire",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                      color: primaryColor,
+                    ),
+                  ),
+                  const SizedBox(height: 70),
+                  Button(text: 'Get Started', onPressed: () => {}),
+                  const SizedBox(height: 32),
+                ],
+              ),
+            )
           ],
         ),
       ),
-    ));
+    );
   }
 }
