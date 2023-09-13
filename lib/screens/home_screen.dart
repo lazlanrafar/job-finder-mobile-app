029da8b60feecc641658ad6d9acbcb0820e8e547
lazlanrafar <lazlanrafar@gmail.com>
2023-09-13 13:36:14 +0700

    slicing header app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:job_finder/shared/theme_shared.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  IconButton(
+                      onPressed: () => {},
+                      icon: Image.asset(
+                        'assets/ic_menu.png',
+                        width: 24,
+                        height: 24,
+                      ),
+                      tooltip: 'Menu',
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.white),
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                        ),
+                      )),
+                  IconButton(
+                    onPressed: () => {},
+                    icon: Image.asset(
+                      'assets/ic_notification.png',
+                      width: 24,
+                      height: 24,
+                    ),
+                    tooltip: 'Notification',
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.white),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "Hello Yeeds!",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w300,
+                  color: primaryColor,
+                ),
+              ),
+              Text(
+                "Find Your Dream Job",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  color: primaryColor,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/shared/theme_shared.dart
+++ lib/shared/theme_shared.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+Color backgroundColor = const Color(0xffF8FAFD);
 Color primaryColor = const Color(0xff081D43);
 Color blueColor = const Color(0xff5077DF);
 
