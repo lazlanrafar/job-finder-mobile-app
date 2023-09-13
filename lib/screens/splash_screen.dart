2217555c246e9973365128083a41ca8ba8ed8f83
lazlanrafar <lazlanrafar@gmail.com>
2023-09-13 12:39:57 +0700

    slicing widgets button

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:job_finder/shared/theme_shared.dart';
+import 'package:job_finder/widgets/button.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -7,27 +8,35 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      padding: EdgeInsets.symmetric(
-        horizontal: defaultMargin,
-        vertical: 32,
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Find the Job You've\nAlways Dreamed of",
-            style: TextStyle(
-                fontSize: 24, fontWeight: FontWeight.w500, color: primaryColor),
-          ),
-          const SizedBox(height: 8),
-          Text(
-            "One of the places where you will find the right job with your field of interest, and you just have to wait for the manager to call you to hire",
-            style: TextStyle(
-                fontSize: 16, fontWeight: FontWeight.w300, color: primaryColor),
-          )
-        ],
+        body: SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+          vertical: 32,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Find the Job You've\nAlways Dreamed of",
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                  color: primaryColor),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              "One of the places where you will find the right job with your field of interest, and you just have to wait for the manager to call you to hire",
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w300,
+                  color: primaryColor),
+            ),
+            const SizedBox(height: 73),
+            Button(text: 'Get Started', onPressed: () => {})
+          ],
+        ),
       ),
     ));
   }
---
+++ lib/widgets/button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:job_finder/shared/theme_shared.dart';
+
+class Button extends StatelessWidget {
+  final String text;
+  final Function() onPressed;
+
+  const Button({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          backgroundColor: blueColor,
+          padding: const EdgeInsets.symmetric(
+            vertical: 20,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
